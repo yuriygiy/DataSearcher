8498d5b5a64f508c2e623c0b1c97a3c0b8a11ac4
yuriygiy <yuriygiy.g@gmail.com>
2020-03-01 22:59:02 +0200

    Fixed app exit param view

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require './search_engine.rb'
 
 user_input = nil
 until user_input == '\q'
-  puts "Please input search params (start param with '--' for search exclusion) type('\q') for exit: "
+  puts "Please input search params (start param with '--' for search exclusion) type('\\q') for exit: "
   user_input = gets.chomp
 
   exit if user_input == '\q'
